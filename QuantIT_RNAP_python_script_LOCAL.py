b7b2977b4f750b7c29478b2c6382848f57ee37ea
mbenj-soundag <109112953+mbenj-soundag@users.noreply.github.com>
2023-11-02 10:06:46 -0700

    Initial filedump for automated cherrypick generation
    These files are run from FluentControl via python.exe
    
    They either take CSV files from the Byonoy reader and use OD values to calculate backdilution values, or they take .xlsx files from a Spark measurement directory in GDrive to calculate backdilution values.

---
+++ QuantIT_RNAP_python_script_LOCAL.py
@@ -0,0 +1,134 @@
+# The following code was rewritten from the OD660_Quant_RNAP_python_script_LOCAL.py script.
+# To run: cd C:/Users/Max/Desktop/RNAP backdilution testing/
+# python QuantIT_RNAP_python_script_LOCAL.py
+
+import os
+import pandas as pd
+import numpy as np
+from datetime import datetime
+from statistics import mean, median, mode
+
+# Step 1: Import FluentControl variables contained in a CSV.
+# Define the CSV file path.
+filepath = "C:/Users/Max/Desktop/RNAP backdilution testing/fluent_control_var_exports.csv"
+
+# Load the CSV file into a DataFrame.
+df = pd.read_csv(filepath ,header=None)
+source_plate_count, norm_conc_1, norm_conc_2, norm_conc_3, norm_conc_4, elution_volume_1, elution_volume_2, elution_volume_3, elution_volume_4 = df.iloc[1, :].astype(float)
+source_plate_count = int(source_plate_count)
+
+# Step 2: Make a dictionary of filepaths and their modification times.
+folder_path = "C:/Users/Max/Desktop/RNAP backdilution testing/measurement_files/"
+file_dates_modified = {os.path.join(folder_path, filename): os.path.getmtime(os.path.join(folder_path, filename)) for filename in os.listdir(folder_path) if filename.endswith(".xlsx")}
+
+# Step 3: Sort filepaths by modification time and select the most recent (based on source_plate_count value imported from FluentControl).
+files_sorted_by_modification_time = dict(sorted(file_dates_modified.items(), key=lambda x: x[1], reverse=True))
+recent_measurement_filepaths = list(files_sorted_by_modification_time)[:source_plate_count+1]
+reordered_measurement_filepaths = recent_measurement_filepaths[::-1]
+standards_filepath = reordered_measurement_filepaths.pop()
+
+# Step 4: Process Standards data from the Lambda rRNA standards assay plate.
+
+standards_df = pd.read_excel(standards_filepath)
+standards_extracted_data = standards_df.iloc[53:61, 1:13].values.astype(float)
+standards_transposed = standards_extracted_data.T
+standards_0 = [standards_transposed[each][0] for each in range(0,12)]
+standards_5 = [standards_transposed[each][1] for each in range(0,12)]
+standards_10 = [standards_transposed[each][2] for each in range(0,12)]
+standards_20 = [standards_transposed[each][3] for each in range(0,12)]
+standards_40 = [standards_transposed[each][4] for each in range(0,12)]
+standards_60 = [standards_transposed[each][5] for each in range(0,12)]
+standards_80 = [standards_transposed[each][6] for each in range(0,12)]
+standards_100 = [standards_transposed[each][7] for each in range(0,12)]
+all_standards_rfu_values = standards_0 + standards_5 + standards_10 + standards_20 + standards_40 + standards_60 + standards_80 + standards_100
+
+lambda_standards_concentrations = [0.0, 5.0, 10.0, 20.0, 40.0, 60.0, 80.0, 100.0]
+lambda_standards_concentrations_array = []
+for each in range(0,len(lambda_standards_concentrations)):
+    for n in range(0,12):
+        lambda_standards_concentrations_array.append(lambda_standards_concentrations[each])
+        
+slope, y_intercept = np.polyfit(all_standards_rfu_values, lambda_standards_concentrations_array, 1)
+
+# Step 5: Process Excel files.
+plate_96_rows = ["A", "B", "C", "D", "E", "F", "G", "H"]
+plate_96_columns = [str(num) for num in range(1, 13)]
+plate_96_wells = [row + column for column in plate_96_columns for row in plate_96_rows]
+
+elution_plate_id_list, backdilution_plate_id_list, filename_list, well_id_list, rfu_data_list = [], [], [], [], []
+
+for loop_count, excel_filepath in enumerate(reordered_measurement_filepaths, start=1):
+    elution_plate_id = f'Elution plate[00{loop_count}]'
+    backdilution_plate_id = f'Backdilution plate[00{loop_count}]'
+    
+    df = pd.read_excel(excel_filepath)
+    extracted_data = df.iloc[53:61, 1:13].values
+    transposed_data = extracted_data.T
+
+    rfu_data_list.extend([float(val) for sublist in transposed_data for val in sublist])
+
+    normalization_concentration = locals()[f'norm_conc_{loop_count}']
+    elution_volume = locals()[f'elution_volume_{loop_count}']
+
+    rna_transfer_volume = elution_volume / 2
+    calculated_rna_conc_list = [(val * slope * 2) + y_intercept for val in rfu_data_list]
+
+    backdilution_volume_list = [round(((rna_transfer_volume * (conc / normalization_concentration)) - rna_transfer_volume), 1) if (conc / normalization_concentration) > 1.0 else 0.0 for conc in calculated_rna_conc_list]
+
+    new_conc_list = [round((conc * rna_transfer_volume) / (rna_transfer_volume + backdilution_volume), 2) for conc, backdilution_volume in zip(calculated_rna_conc_list, backdilution_volume_list)]
+
+    num_values = 96
+    elution_plate_id_list.extend([elution_plate_id] * num_values)
+    backdilution_plate_id_list.extend([backdilution_plate_id] * num_values)
+
+    filename_list.extend([os.path.basename(excel_filepath)] * num_values)
+    well_id_list.extend(plate_96_wells[:num_values])
+
+metadata_df = pd.DataFrame({
+    'Measurement file name': filename_list,
+    'Well ID': well_id_list,
+    'RNA Elution Plate #': elution_plate_id_list,
+    'RFU': rfu_data_list,
+    'Elution Plate RNA conc (ng/ul)': calculated_rna_conc_list,
+    'Backdilution volume needed for normalization (ul)': backdilution_volume_list,
+    'Backdilution Plate RNA conc (ng/ul)': new_conc_list,
+    'Backdilution Plate #': backdilution_plate_id_list
+})
+
+# Create lists for source plate and well names
+source_plate_name = 'Water'
+source_well_name = 'A1'
+source_plate_name_list = [source_plate_name] * len(backdilution_plate_id_list)
+source_well_name_list = [source_well_name] * len(backdilution_plate_id_list)
+
+# Create the cherrypick DataFrame
+cherrypicking_df = pd.DataFrame({
+    'SOURCE PLATE': source_plate_name_list,
+    'SOURCE WELL NAME': source_well_name_list,
+    'DESTINATION PLATE': backdilution_plate_id_list,
+    'DESTINATION WELL NAME': well_id_list,
+    'VOLUME (ul)': backdilution_volume_list
+})
+
+# Format the current timestamp for file naming
+current_time = datetime.now()
+reformatted_datetime_string = current_time.strftime("%Y-%m-%d %H-%M-%S")
+
+# Define storage and working directories
+working_directory = "C:/Users/Max/Desktop/RNAP backdilution testing/working directory/"
+storage_directory = "C:/Users/Max/Desktop/RNAP backdilution testing/automated backdilution cherrypick logs/"
+
+# Define file names
+metadata_csv_filename = f"{reformatted_datetime_string}_Fluent backdilution_metadata.csv"
+backup_cherrypick_csv_filename = f"{reformatted_datetime_string}_Fluent backdilution_cherrypick.csv"
+working_cherrypick_csv_filename = "Fluent_backdilution_cherrypick.csv"
+
+# Define file paths
+metadata_file_path = os.path.join(storage_directory, metadata_csv_filename)
+backup_cherrypick_file_path = os.path.join(storage_directory, backup_cherrypick_csv_filename)
+working_cherrypick_file_path = os.path.join(working_directory, working_cherrypick_csv_filename)
+
+# Export DataFrames to CSV
+metadata_df.to_csv(metadata_file_path, index=False)
+cherrypicking_df.to_csv(backup_cherrypick_file_path, index=False)
+cherrypicking_df.to_csv(working_cherrypick_file_path, index=False)
---
+++ RNAP_python_script_v3_LOCAL.py
@@ -0,0 +1,203 @@
+# To run: cd C:/Users/Max/Desktop/RNAP backdilution testing/ then python RNAP_python_script_v3_LOCAL.py
+
+# Import necessary libraries.
+import os
+import pandas as pd
+import numpy as np
+from datetime import datetime
+
+# Step 1: Import FluentControl variables contained in a CSV
+
+#ENTER FLUENT LOCAL DIRECTORY CONTAINING CSV EXPORT HERE
+#filepath = "C:/Users/Tecan/Desktop/Tecan Fluent desktop files/RNAP data management/rnap_automated_backdilution_cherrypick_inputs.csv"
+filepath = "C:/Users/Max/Desktop/RNAP backdilution testing/store/fluent_control_var_exports.csv"
+df = pd.read_csv(filepath, skiprows=0, header=None)
+source_plate_count = int(df.iloc[1,0])
+norm_conc_1 = float(df.iloc[1,1])
+norm_conc_2 = float(df.iloc[1,2])
+norm_conc_3 = float(df.iloc[1,3])
+norm_conc_4 = float(df.iloc[1,4])
+elution_volume_1 = float(df.iloc[1,5])
+elution_volume_2 = float(df.iloc[1,6])
+elution_volume_3 = float(df.iloc[1,7])
+elution_volume_4 = float(df.iloc[1,8])
+
+# Step 2: Make a dictionary of all filepaths and their creation times from your directory of choice, as key:value pairs.
+
+#ENTER FLUENT LOCAL DIRECTORY CONTAINING BYONOY MEASUREMENT FILES HERE
+#folder_path = "C:/Users/Tecan/Desktop/Tecan Fluent desktop files/RNAP data management/byonoy automated csv exports/"
+folder_path = "C:/Users/Max/Desktop/RNAP backdilution testing/measurement_files/"
+file_dates_modified = {}
+
+for filename in os.listdir(folder_path):
+    file_path = os.path.join(folder_path, filename)
+    date_modified = os.path.getmtime(file_path)
+    if filename.endswith(".csv"):
+        file_dates_modified.update({file_path : date_modified})
+
+# Step 3: Sort all filepaths by creation date, then only pick the sourcePlateCount number of most recent items.
+files_sorted_by_modification_time = dict(sorted(file_dates_modified.items(), key=lambda x:x[1],reverse=True))
+recent_run_filepaths = list(files_sorted_by_modification_time.keys())[:source_plate_count]
+
+# Step 4: Grab data from each CSV file, in order, importing it into a dataframe.
+
+# Create a columnwise list of wells in a 96-well plate.
+plate_96_rows = ["A","B","C","D","E","F","G","H"]
+plate_96_columns = [str(num) for num in range(1,13)]
+plate_96_wells = []
+for column in plate_96_columns:
+    for row in plate_96_rows:
+        well = row + column
+        plate_96_wells.append(well)
+
+# Initialize lists needed to build final dataframes for CSV export.
+filename_list = []
+elution_plate_id_list = []
+backdilution_plate_id_list = []
+well_id_list = []
+od_data_list = []
+calculated_rna_conc_list = []
+backdilution_volume_list = []
+new_conc_list = []
+
+# Adjust these values as needed, listed in order from A12-H12
+standards_xylose_concentrations = [0.0, 20.0, 40.0, 60.0, 80.0, 100.0, 150.0, 200.0]
+
+loop_count = 1
+for csv_file in recent_run_filepaths:
+    elution_plate_id = f'Elution plate[00{loop_count}]'
+    backdilution_plate_id = f'Backdilution plate[00{loop_count}]'
+    
+    # Import CSV to dataframe.
+    df = pd.read_csv(csv_file, skiprows=0, header=None)
+    
+    # Grab only relevant data.
+    extracted_data = df.iloc[1:9, 1:13].values
+    
+    # Pull Xylose Standards data from column 12.
+    standards_od_data = list(df.iloc[1:9,12])
+    standards_od_data_floats = []
+    for each in range(0,len(standards_od_data)):
+        standards_od_data_floats.append(float(standards_od_data[each]))
+    
+    # Use standards data from column 12 to calculate LOBF.
+    x = np.array(standards_od_data_floats)
+    y = np.array(standards_xylose_concentrations)
+    slope, y_intercept = np.polyfit(x, y, 1)
+    
+    # Transpose all OD data to stack it column-wise: each column becomes a list. Total data = a list containing 12 lists.
+    transposed_data = extracted_data.T
+    
+    # Flatten the array containing OD data and add it to a new running list: 12 lists are merged end-to-end. Total data = 1 longer list.
+    flattened_data = transposed_data.flatten()
+    od_data_list.extend(flattened_data)
+    
+    # Change elution volumes for subsequent backdilution volume calculation math, per plate.
+    if loop_count == 1:
+        normalization_concentration = norm_conc_1
+        elution_volume = elution_volume_1
+    elif loop_count == 2: 
+        normalization_concentration = norm_conc_2
+        elution_volume = elution_volume_2
+    elif loop_count == 3:
+        normalization_concentration = norm_conc_3 
+        elution_volume = elution_volume_3
+    elif loop_count == 4:
+        normalization_concentration = norm_conc_4
+        elution_volume = elution_volume_4
+    rna_transfer_volume = elution_volume / 2 # This can be user-specified in the future
+    
+    # Use OD data and LOBF from standards data to calculate RNA concentrations.
+    for each in range(0,len(flattened_data)):
+        calculated_rna_conc = (float(flattened_data[each]) * slope) + y_intercept
+        calculated_rna_conc_list.append(calculated_rna_conc)
+
+        # Then use calculated RNA concentration data and norm_conc values to get backdilution volumes.
+        backdilution_ratio = calculated_rna_conc_list[each] / normalization_concentration
+        if backdilution_ratio <= 1.0:
+            backdilution_volume = 0.0
+        elif backdilution_ratio > 1.0:
+            backdilution_volume = round(((rna_transfer_volume * backdilution_ratio) - rna_transfer_volume),1)
+
+        backdilution_volume_list.append(backdilution_volume)
+        
+        # Record all new conc values in a list.
+        new_concentration = round(((calculated_rna_conc_list[each] * rna_transfer_volume) / (rna_transfer_volume + backdilution_volume)),2)
+        new_conc_list.append(new_concentration)
+            
+    # Add the plate IDs for each value in the data (96 values).
+    num_values = transposed_data.size
+    elution_plate_id_list.extend([elution_plate_id] * num_values)
+    backdilution_plate_id_list.extend([backdilution_plate_id] * num_values)
+    
+    # Add the source CSV filename for each value in the data (96 values).
+    # Change 76 to some other number as needed. This pulls just the filename from the filepath in recent_run_filepaths.
+    #filename = recent_run_filepaths[loop_count-1][76:]
+    filename = recent_run_filepaths[loop_count-1][47:]
+    filename_list.extend([filename] * num_values)
+    #print(filename_list)
+    
+    # Add the well ID for each value in the CSV to the well_id_list.
+    well_id_list.extend(plate_96_wells[:num_values])
+    #print(well_id_list)
+
+    loop_count += 1
+
+# Create a combined CSV file containing all metadata for recordkeeping purposes.
+metadata_df = pd.DataFrame({
+    'Filename': filename_list,
+    'Elution Plate ID' : elution_plate_id_list,
+    'Well ID' : well_id_list,
+    'OD660' : od_data_list,
+    'RNA conc (ng/ul)' : calculated_rna_conc_list,
+    'Backdilution volume (ul)' : backdilution_volume_list,
+    'New concentration (ng/ul)' : new_conc_list,
+    'Backdilution Plate ID' : backdilution_plate_id_list
+})
+
+# Create lists of proper length to fill in slots for source plates and source wells in the cherrypick df.
+
+source_plate_name = 'Water'
+source_well_name = 'A1'
+source_plate_name_list = []
+source_well_name_list = []
+for each in range(0,len(backdilution_plate_id_list)):
+    source_plate_name_list.append(source_plate_name)
+    source_well_name_list.append(source_well_name)
+
+cherrypicking_df = pd.DataFrame({
+    'SOURCE PLATE' : source_plate_name_list,
+    'SOURCE WELL NAME' : source_well_name_list,
+    'DESTINATION PLATE' : backdilution_plate_id_list,
+    'DESTINATION WELL NAME' : well_id_list,
+    'VOLUME (ul)' : backdilution_volume_list
+})
+
+# Turn current timestamp into a string for file naming purposes.
+current_time = datetime.now()
+current_time_as_string = str(current_time)
+reformatted_datetime_string = ""
+for char in current_time_as_string[0:19]:
+    if char == ':':
+        new_char = '-'
+        reformatted_datetime_string += new_char
+    else:
+        reformatted_datetime_string += char
+
+# Now finally export the metadata & cherrypick dfs as CSVs into storage folders and working folders.
+#working_directory = "C:/Users/Tecan/Desktop/Tecan Fluent desktop files/RNAP data management/working directory/"
+#storage_directory = "C:/Users/Tecan/Desktop/Tecan Fluent desktop files/RNAP data management/automated backdilution cherrypick logs/"
+working_directory = "C:/Users/Max/Desktop/RNAP backdilution testing/working directory/"
+storage_directory = "C:/Users/Max/Desktop/RNAP backdilution testing/automated backdilution cherrypick logs/"
+
+metadata_csv_filename = reformatted_datetime_string + "_Fluent backdilution_metadata"           
+output_file_path = storage_directory + metadata_csv_filename + ".csv"
+metadata_df.to_csv(output_file_path, index=False)
+
+backup_cherrypick_csv_filename = reformatted_datetime_string + "_Fluent backdilution_cherrypick"        
+output_file_path = storage_directory + backup_cherrypick_csv_filename + ".csv"
+cherrypicking_df.to_csv(output_file_path, index=False)
+
+working_cherrypick_csv_filename = "Fluent_backdilution_cherrypick"           
+output_file_path = working_directory + working_cherrypick_csv_filename + ".csv"
+cherrypicking_df.to_csv(output_file_path, index=False)
